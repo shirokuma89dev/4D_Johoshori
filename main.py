29739b185105cd6b79159659a9e7569e663fd309
shirokuma89dev <code@shirokuma89.dev>
2024-08-25 13:40:29 +0900

    うー

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
-# 概要:
-# 1. 文字列の中のアルファベット以外をスペースに置き換える
-# 2. 文字列の末尾に" "を追加する
-# 3. keyに" "を追加する
-# 4. 検索を行う
-
-skip = [0 for i in range(256)]
-
-def table(key):
-    key = key + " "
-    keyLength = len(key)
-
-    for k in range(256):
-        skip[k] = keyLength
-
-    for k in range(keyLength - 1):
-        skip[ord(key[k])] = keyLength - 1 - k
-    
-    return key
 
 
-def search(s, text, key):
-    keyLength = len(key)
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-    # If not alphabetic (number, symbol, whitespace, etc.),
-    # replace with a space:
-    for i in range(len(text)):
-        if not text[i].isalpha():
-            text = text[:i] + " " + text[i + 1 :]
+import numpy as np
 
-    text = text + " "  # Append a space to the end of the text
+# シグモイド関数とその導関数
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-    currentIndex = s + keyLength - 1
+def sigmoid_derivative(x):
+    return x * (1 - x)
 
-    while currentIndex < len(text):
-        if text[currentIndex] == key[keyLength - 1]:
-            if text[currentIndex - keyLength + 1 : currentIndex + 1] == key:
-                return currentIndex - keyLength + 1
+def compute_output_with_learned_weights(input_data):
+    # 順伝播
+    layer1 = sigmoid(np.dot(input_data, weights1))
+    output = sigmoid(np.dot(layer1, weights2))
+    return output
 
-        currentIndex += skip[ord(text[currentIndex])]
+x = [
+    [0, 0], [0, 0.5], [0, 1],
+    [0.5, 0], [0.5, 0.5], [0.5, 1],
+    [1, 0], [1, 0.5], [1, 1]
+]
 
-    return -1
+# 勾配降下法？で頑張って求めてみました（あんまりよくわかっていない）
+weights1 = [
+    [1.43642476, 1.44389249, -23.87867004, -4.61102169],
+    [0.190902, 0.18345505, 12.62101136, 3.092514],
+]
+weights2 = [[-5.8954511], [-5.82563046], [-32.85718252], [35.82311971]]
 
+# 出力の計算
+outputs = [compute_output_with_learned_weights(np.array(i)) for i in x]
 
-text = "This is a pen. That is a pencil."
-key = "pen"
-key = table(key)
+# 出力のプロット
+plotx = [i[0] for i in x]
+ploty = [i[1] for i in x]
+plotsize = [300.0 if out > 0.5 else 0.0 for out in outputs]
 
-found_index = search(0, text, key)
-while found_index != -1:
-    print("{:s}".format(text[found_index:]))
-    found_index = search(found_index + len(key), text, key)
+plt.scatter(plotx, ploty, s=plotsize)
+plt.title('Neural Network Output')
+plt.xlabel('Input Index')
+plt.ylabel('Output Value')
+plt.grid(True)
+plt.show()
