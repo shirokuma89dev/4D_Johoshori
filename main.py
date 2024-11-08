13448055e9d7b3e4909a5b3b102429bae53e1300
shirokuma89dev <code@shirokuma89.dev>
2024-11-08 12:01:17 +0900

    ✅ 第五回課題

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-def push(n):
-    stack.append(n)
+def find_matching_bracket_index(bracket_string, num):
+    MAX_SIZE = 128
+    stack = [0 for _ in range(MAX_SIZE)]
+    stack_pointer = 0
 
-def pop():
-    try:
-        return 0, stack.pop()
-    except IndexError:
-        return -1, None
+    for index in range(len(bracket_string)):
+        char = bracket_string[index]
+        if char == "(":
+            stack[stack_pointer] = index
+            stack_pointer += 1
+        elif char == ")":
+            stack_pointer -= 1
+            if stack_pointer < 0:
+                return None
+            if stack[stack_pointer] == num:
+                return index
+            if index == num:
+                return stack[stack_pointer]
 
-if __name__ == "__main__":
-    stack = []
-    while (c := input("i;push o;pop; e;end ?")) != "e":
-        if c == "i":
-            data = input("data?")
-            ret = push(data)
-        elif c == "o":
-            ret, n = pop()
-            if ret == -1:
-                print("スタックが空です")
-            else:
-                print("pop -->  {:s}".format(n))
-        elif c == "e":
-            break
+    return None
+
+
+bracket_string = "((()(()())))"
+num = 3
+
+matching_bracket_index = find_matching_bracket_index(bracket_string, num)
+print(
+    num,
+    "番目の括弧に対応する括弧は",
+    matching_bracket_index,
+    "番目の",
+    bracket_string[matching_bracket_index],
+    "です。",
+)
